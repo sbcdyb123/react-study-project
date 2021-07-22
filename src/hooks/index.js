b927e832f54d04c1b323d94a6c0e331b213ca973
fanglong <337956017@qq.com>
2021-07-22 23:41:29 +0800

    feat: 添加自定义hook，1.防抖2.组件初始化

---
+++ src/hooks/index.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+
+/*******
+ * @Date: 2021-07-22 23:16:15
+ * @name: 方龙
+ * @description: 组件初始化只执行一次 hook
+ * @param {function} callback
+ * @return {*}
+ */
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+}
+/*******
+ * @Date: 2021-07-22 23:32:39
+ * @name: 方龙
+ * @description: 将state依赖值进行防抖处理
+ * @param {any} value 需要进行防抖处理的依赖值
+ * @param {number} delay 延迟毫秒
+ * @return {any}
+ */
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setBebounceValue] = useState(value)
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => {
+      setBebounceValue(value)
+    }, delay)
+    // 每次在上一个useEffect处理完以后清除定时器
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [value, delay])
+  return debounceValue
+}
--- src/screens/ProjectList/index.jsx
+++ src/screens/ProjectList/index.jsx
@@ -3,30 +3,32 @@ import { SearchPanel } from './SearchPanel'
 import { useState, useEffect } from 'react'
 import * as qs from 'qs'
 import { cleanObject } from 'utils'
+import { useDebounce, useMount } from 'hooks'
 const apiUrl = process.env.REACT_APP_API_URL
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: '',
     personId: '',
   })
+  const debounceParam = useDebounce(param, 500)
   const [list, setList] = useState([])
   const [users, setUsers] = useState([])
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (response) => {
-        if (response.ok) {
-          setList(await response.json())
-        }
-      },
-    )
-  }, [param])
-  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`,
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json())
+      }
+    })
+  }, [debounceParam])
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
   return (
     <div>
       <SearchPanel
